07686cfce75d1d0267343ff7c2a8493a59265b1b
Johnathan Grayson <ajgrayson@gmail.com>
2013-09-07 11:54:51 +1200

    First commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Weava::Application.config.secret_key_base = 'b52e3992cc6d6c9fb3b247ccb52b80689454eaa64bb02f156c571a407b0e0e4e9d0b32e1528221b92b9f73845281eeee6c4f2bdf80951950d1f1e935f817eac6'
