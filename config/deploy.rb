4c8139f05b4b85d3a304e3b17374e5fea5a2a2dd
Johnathan Grayson <ajgrayson@gmail.com>
2013-10-05 17:31:03 +1300

    Updated to use the correct user

--- config/deploy.rb
+++ config/deploy.rb
@@ -41,7 +41,7 @@ after "deploy:restart", "deploy:cleanup"
 
 # Server specific
 # ----------------
-set :user, "deploy"
+set :user, "apps"
 server "app.weava.io", :web, :app, :db, :primary => true
 set :deploy_to, "/home/apps/#{application}"
 set :rails_env, "production"
