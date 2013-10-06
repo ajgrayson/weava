61057756f439d4fa3db1d685201671317a8f010a
Johnathan Grayson <ajgrayson@gmail.com>
2013-10-06 19:25:09 +1300

    Added migrate to deploy config

--- config/deploy.rb
+++ config/deploy.rb
@@ -33,7 +33,7 @@ default_run_options[:pty] = true
 load 'deploy/assets'
 
 # if you want to clean up old releases on each deploy uncomment this:
-after "deploy:restart", "deploy:cleanup"
+after "deploy:migrate", "deploy:restart", "deploy:cleanup"
 
 # if you're still using the script/reaper helper you will need
 # these http://github.com/rails/irs_process_scripts
